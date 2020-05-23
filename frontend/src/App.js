ddba6b5ff5c0837cd58fd14ae97131d0519ec088
Jackie098 <carlosmiranda19122@gmail.com>
2020-05-23 19:23:03 -0300

    catching images and listing them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
+import { uniqueId } from 'lodash'; //Para gerar ID único
+import filesize from 'filesize';  //Para controlar a nomenclatura do tamanho de uma imagem
 
 import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
@@ -6,18 +8,57 @@ import { Container, Content } from './styles';
 import Upload from './components/Upload';
 import FileList from './components/FileList';
 
-function App() {
-  return (
-    <>
-      <Container>
-        <Content>
-          <Upload />
-          <FileList />
-        </Content>
-        <GlobalStyle />
-      </Container>
-    </>
-  );
+class App extends Component {
+  // const [uploadedFiles, arrayFiles] = useState([]);
+  state = {
+    uploadedFiles: [],
+  };
+
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({  //criando um array de objetos
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null,
+    }))
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    })
+  };
+
+  render(){
+    const { uploadedFiles } = this.state;
+
+    return (
+      <>
+        <Container>
+          <Content>
+            <Upload onUpload={this.handleUpload} />
+            {/** Para retornar o componente da lista, primeiro haverá uma
+             * verificação para averiguar se na lista (UploadedFiles), tem mais
+             * de 0 itens.
+             * 
+             * Utilizando o !! ao inves de retornar a quantidade de itens, retorna
+             * TRUE para > 0 itens e FALSE para = 0 itens na lista
+             */
+             
+              !!uploadedFiles.length && (
+                <FileList files={uploadedFiles}/>
+              )
+             }
+
+          </Content>
+          <GlobalStyle />
+        </Container>
+      </>
+    );
+  }
 }
 
 export default App;
--- frontend/src/components/FileList/index.js
+++ frontend/src/components/FileList/index.js
@@ -4,39 +4,53 @@ import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
 
 import { Container, FileInfo, Preview } from './styles';
 
-const FileList = () => (
+const FileList = ({ files }) => (
   <Container>
-    <li>
-      <FileInfo>
-        <Preview src="http://localhost:3000/files/d9fe483e486648c87735364695c9418f-logo.jpeg" />
+    {files.map(uploadedFile => (
+      <li key={uploadedFile.id}>
+        <FileInfo>
+          <Preview src={uploadedFile.preview} />
+          <div>
+            <strong>{uploadedFile.name}</strong>
+            <span>
+              {uploadedFile.readableSize}{" "}
+
+              {!!uploadedFile.url && (
+                <button onClick={() => {}}>Excluir</button>
+              )}
+            </span>
+          </div>
+        </FileInfo>
+      
         <div>
-          <strong>nomeImagem.png</strong>
-          <span>180kb <button onClick={() => {}}>Excluir</button></span>
+          {!uploadedFile.uploaded && !uploadedFile.error && (
+            <CircularProgressbar
+            styles={{
+              root: { width: 24 },
+              path: { stroke: '#7159c1'},
+            }}
+            strokeWidth={10}
+            value={uploadedFile.progress}
+            />
+          )}
+
+          {
+            uploadedFile.url && (
+              <a 
+              href="http://localhost:3000/files/d9fe483e486648c87735364695c9418f-logo.jpeg"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+                <MdLink style={{ marginLeft: 8 , marginRight: 8}} size={24} color="#222" />
+              </a>
+            )}
+
+            { uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" /> }
+            
+            { uploadedFile.error && <MdError size={24} color="#e57878" /> }        
         </div>
-      </FileInfo>
-    
-      <div>
-        <CircularProgressbar
-          styles={{
-            root: { width: 24 },
-            path: { stroke: '#7159c1'},
-          }}
-          strokeWidth={10}
-          value={65}
-          />
-
-          <a 
-            href="http://localhost:3000/files/d9fe483e486648c87735364695c9418f-logo.jpeg"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <MdLink style={{ marginLeft: 8 , marginRight: 8}} size={24} color="#222" />
-          </a>
-
-          <MdCheckCircle size={24} color="#78e5d5" />
-          <MdError size={24} color="#e57878" />        
-      </div>
-    </li>
+      </li>
+    ))}
   </Container>
 );
 
