37bebcf8b6704d27a61b6af875cb1cae1bd3349f
Jackie098 <carlosmiranda19122@gmail.com>
2020-05-24 01:42:43 -0300

    resolving merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -70,7 +70,6 @@ class App extends Component {
 
     data.append('file', uploadedFile.file, uploadedFile.name);
 
-<<<<<<< HEAD
     api
       .post('uploads', data, {
         onUploadProgress: e => {
@@ -107,19 +106,6 @@ class App extends Component {
     this.state.uploadedFiles.forEach(file => URL.revokeObjectURL(file.preview));
   }
 
-=======
-    api.post('uploads', data, {
-      onUploadProgress: e => {
-        const progress = parseInt(Math.round((e.loaded * 100) / e.total));
-      
-        this.updateFile(uploadedFile.id, {
-          progress,
-        })
-      }
-    });
-  }
-
->>>>>>> 2c159c2aa1a334da6a7b8160ffab3090740fec6b
   render(){
     const { uploadedFiles } = this.state;
 
