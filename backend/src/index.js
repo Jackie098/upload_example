6174351a589b467984ce0c5faca87fb3e3f49add
Jackie098 <carlosmiranda19122@gmail.com>
2020-05-23 21:44:58 -0300

    added cors in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
 const path = require('path');
+const cors = require('cors');
 
 require('./database');
 
@@ -10,6 +11,7 @@ const routes = require("./routes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
